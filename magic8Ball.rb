0cd169f1a52da9ead64b5cbc9da6af120e642507
Trevor Howard <trevor@hoverstate.com>
2018-11-15 15:19:41 -0700

    play again added, new lines inserted

--- magic8Ball.rb
+++ magic8Ball.rb
@@ -1,6 +1,3 @@
-puts "What is your Magic 8 Ball question?"
-userQuestion = gets.chomp
-
 class R_Array
   def self.newArray 
     ["As I see it, yes", "Ask again later", "Better not tell you now", "Cannot predict now", "Concentrate and ask again", "Don’t count on it", "It is certain", "It is decidedly so", "Most likely", "My reply is no", "My sources say no", "Outlook good", "Outlook not so good", "Reply hazy, try again", "Signs point to yes", "Very doubtful", "Without a doubt", "Yes", "Yes, definitely", "You may rely on it."]
@@ -13,45 +10,53 @@ class AnimArr
     userResponse = gets.chomp
     STDOUT.puts anim("\rConsulting the heavens...")
     sleep 2
-    print R_Array.newArray.sample
+    print R_Array.newArray.sample + "\n"
+    play_again
   end
 end
 class AnimArr2
   def self.qAnimArr2
     STDOUT.puts anim("\rConsulting the heavens...")
     sleep 2
-    print R_Array.newArray.sample
+    print R_Array.newArray.sample + "\n"
+    play_again
   end
 end
 
 def anim(string)
   string.each_char do |n|
   print n
-  charWait = rand(0.05..0.15)
+  charWait = rand(0.03..0.07)
   sleep(charWait)
   end
 end
 
+def play_again
+	sleep(1.5)
+	print "Need more answers?" + "\n"
+	user_choice = gets.chomp.downcase
+	magic_8_ball if user_choice != 'n' || user_choice != 'N'
+end
+
+def magic_8_ball
+  puts "What is your Magic 8 Ball question?"
+  userQuestion = gets.chomp
+
   if userQuestion == "" || userQuestion == " "
-  	print "Are you there?\n"
-  	userResponse = gets.chomp
-  	if userResponse == 'y' || userResponse == 'yes' || userResponse == 'yeah'
-  		anim("Well then...\n")
+    print "Are you there?\n"
+    userResponse = gets.chomp
+    if userResponse == 'y' || userResponse == 'yes' || userResponse == 'yeah'
+      anim("Well then...\n")
       sleep 1
       AnimArr.qAnimArr
-  	else userResponse == 'n' || userResponse =='no' || userResponse == 'nah'
-  		anim("Don't lie to me... Come on now...\n")
+    else userResponse == 'n' || userResponse =='no' || userResponse == 'nah'
+      anim("Don't lie to me... Come on now...\n")
       sleep 1.5
       AnimArr.qAnimArr
-  	end
+    end
   else
     AnimArr2.qAnimArr2
   end
+end
 
-#write an array that stores comments that can be called upon and printed to the terminal.
-
-# puts "Give me a comment"
-# comment = gets.chomp
-# newComment = Array[]
-# newComment << (comment)
-# print newComment
+magic_8_ball
