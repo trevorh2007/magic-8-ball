0ad6dd68b90ec8874f8c9dbe3795fac28f6f534f
Trevor Howard <trevor@hoverstate.com>
2018-11-15 15:42:05 -0700

    replace needless classes

--- magic8Ball.rb
+++ magic8Ball.rb
@@ -1,26 +1,12 @@
-class R_Array
-  def self.newArray 
-    ["As I see it, yes", "Ask again later", "Better not tell you now", "Cannot predict now", "Concentrate and ask again", "Don’t count on it", "It is certain", "It is decidedly so", "Most likely", "My reply is no", "My sources say no", "Outlook good", "Outlook not so good", "Reply hazy, try again", "Signs point to yes", "Very doubtful", "Without a doubt", "Yes", "Yes, definitely", "You may rely on it."]
-  end
+def response_array
+  ["As I see it, yes", "Ask again later", "Better not tell you now", "Cannot predict now", "Concentrate and ask again", "Don’t count on it", "It is certain", "It is decidedly so", "Most likely", "My reply is no", "My sources say no", "Outlook good", "Outlook not so good", "Reply hazy, try again", "Signs point to yes", "Very doubtful", "Without a doubt", "Yes", "Yes, definitely", "You may rely on it."]
 end
 
-class AnimArr
-  def self.qAnimArr
-    puts "What is your Magic 8 Ball question?"
-    userResponse = gets.chomp
-    STDOUT.puts anim("\rConsulting the heavens...")
-    sleep 2
-    print R_Array.newArray.sample + "\n"
-    play_again
-  end
-end
-class AnimArr2
-  def self.qAnimArr2
-    STDOUT.puts anim("\rConsulting the heavens...")
-    sleep 2
-    print R_Array.newArray.sample + "\n"
-    play_again
-  end
+def animate_array
+  STDOUT.puts anim("\rConsulting the heavens...")
+  sleep 2
+  print response_array.sample + "\n"
+  play_again
 end
 
 def anim(string)
@@ -34,8 +20,8 @@ end
 def play_again
 	sleep(1.5)
 	print "Need more answers?\n"
-	user_choice = gets.chomp.downcase
-	magic_8_ball if user_choice != 'n' || user_choice != 'N'
+  user_choice = gets.chomp.downcase
+  magic_8_ball unless ['n', 'no'].include?(user_choice)
 end
 
 def magic_8_ball
@@ -49,14 +35,14 @@ def magic_8_ball
     if userResponse == 'y' || userResponse == 'yes' || userResponse == 'yeah'
       anim("Well then...\n")
       sleep 1
-      AnimArr.qAnimArr
+      magic_8_ball
     else userResponse == 'n' || userResponse =='no' || userResponse == 'nah'
       anim("Don't lie to me... Come on now...\n")
       sleep 1.5
-      AnimArr.qAnimArr
+      magic_8_ball
     end
   else
-    AnimArr2.qAnimArr2
+    animate_array
   end
 end
 
